0cd311c2d456f7efa5f592403066bbf7454a9303
petrix007 <petr.ml.legat@gmail.com>
2022-11-29 22:31:20 +0100

    0.22

--- src/main/java/Projekt/sbirka/UI/UIController.java
+++ src/main/java/Projekt/sbirka/UI/UIController.java
@@ -1,13 +1,59 @@
 package Projekt.sbirka.UI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+@Component
 public class UIController implements Initializable {
+
+    @FXML
+    private BorderPane login;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private BorderPane menu;
+
+    @FXML
+    private Button menuBtn;
+    @FXML
+    private BorderPane pridat;
+
+    @FXML
+    private Button pridatBtn;
+
+
+    @FXML
+    private Text titleTxt;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    private void handleClick(ActionEvent event) {
+        if (event.getSource() == loginBtn){
+            login.toFront();
+            titleTxt.setText("LOGIN");
+        }
+        if (event.getSource() == menuBtn){
+            titleTxt.setText("MENU");
+            menu.toFront();
+        }
+        if (event.getSource() == pridatBtn){
+            pridat.toFront();
+            titleTxt.setText("PŘIDAT");
+        }
+
+    }
+
 }

70b50e178df63a7b31d77e6f60e5889e7c264ba4
petrix007 <petr.ml.legat@gmail.com>
2022-12-02 23:09:28 +0100

    0.23

--- src/main/java/Projekt/sbirka/UI/UIController.java
+++ src/main/java/Projekt/sbirka/UI/UIController.java
@@ -3,11 +3,17 @@ package Projekt.sbirka.UI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 @Component
@@ -24,15 +30,31 @@ public class UIController implements Initializable {
 
     @FXML
     private Button menuBtn;
-    @FXML
-    private BorderPane pridat;
 
     @FXML
-    private Button pridatBtn;
+    private BorderPane pridatModel;
 
+    @FXML
+    private Button pridatModelbtn;
+
+    @FXML
+    private BorderPane pridatSbirku;
+
+    @FXML
+    private Button pridatSbirkuBtn;
+
+    @FXML
+    private BorderPane sbirky;
+
+    @FXML
+    private Button sbirkyBtn;
+
+    @FXML
+    private BorderPane titleColor;
 
     @FXML
     private Text titleTxt;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -44,14 +66,27 @@ public class UIController implements Initializable {
         if (event.getSource() == loginBtn){
             login.toFront();
             titleTxt.setText("LOGIN");
+            titleColor.setBackground(new Background(new BackgroundFill(Color.rgb(37, 109, 133, 1), CornerRadii.EMPTY, Insets.EMPTY)));
         }
         if (event.getSource() == menuBtn){
             titleTxt.setText("MENU");
+            titleColor.setBackground(new Background(new BackgroundFill(Color.rgb(6, 40, 61, 1), CornerRadii.EMPTY, Insets.EMPTY)));
             menu.toFront();
         }
-        if (event.getSource() == pridatBtn){
-            pridat.toFront();
-            titleTxt.setText("PŘIDAT");
+        if (event.getSource() == pridatSbirkuBtn){
+            pridatSbirku.toFront();
+            titleTxt.setText("PŘIDAT SBÍRKU");
+            titleColor.setBackground(new Background(new BackgroundFill(Color.rgb(48, 71, 94, 1), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
+        if (event.getSource() == pridatModelbtn){
+            pridatModel.toFront();
+            titleTxt.setText("PŘIDAT MODEL");
+            titleColor.setBackground(new Background(new BackgroundFill(Color.rgb(49, 85, 143, 1), CornerRadii.EMPTY, Insets.EMPTY)));
+        }
+        if (event.getSource() == sbirkyBtn){
+            sbirky.toFront();
+            titleTxt.setText("SBÍRKY");
+            titleColor.setBackground(new Background(new BackgroundFill(Color.rgb(17, 43, 115, 1), CornerRadii.EMPTY, Insets.EMPTY)));
         }
 
     }
